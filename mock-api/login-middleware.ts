2979b19a2154dc1ad74b6673e27972a965ebf4be
MJulia111 <julia.m241992@gmail.com>
2023-04-13 22:56:11 +0200

    fixed json-server

--- mock-api/login-middleware.ts
+++ mock-api/login-middleware.ts
@@ -1,9 +1,9 @@
 module.exports = (req, res, next) => {
   if (req.method == "POST" && req.path == "/login") {
     if (req.body.user === "admin" && req.body.password === "123456789") {
-      res.status(200).json({ role: req.body.role, name: req.body.name });
+      res.status(200).json({ role: "admin", name: "Иван Иванов" });
     } else if (req.body.user === "user" && req.body.password === "123456789") {
-      res.status(200).json({ role: req.body.role, name: req.body.name });
+      res.status(200).json({ role: "user", name: "Петр Петров" });
     } else {
       res.status(400).json({ message: "wrong password" });
     }
